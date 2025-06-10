a596c04a4a4495e40a9c51062042944798f4a84e
Xiomara Andrea <xiomaraa.pulido@gmail.com>
2025-06-10 14:30:07 +0200

    Fix user detail

--- models/UserAdminModel.php
+++ models/UserAdminModel.php
@@ -41,7 +41,7 @@ class UserAdminModel {
     }
 
     public function findById($id) {
-        $stmt = $this->db->prepare("SELECT * FROM adminusers WHERE id = :id");
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
